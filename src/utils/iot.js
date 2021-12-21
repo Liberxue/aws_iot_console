7382f3bd7282e9cc6c749dddf57d85a8bfdf9351
Liberxue <5831248+Liberxue@users.noreply.github.com>
2021-12-21 11:19:01 +0800

    Update iot.js

--- src/utils/iot.js
+++ src/utils/iot.js
@@ -1,37 +1,33 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-  // accessKeyId: process.env.ACCESS_KEY_ID,
-  // secretAccessKey: process.env.SECRET_ACCESS_KEY,
-  // region: process.env.REGION
-  accessKeyId: 'AKIAVVYGLS733LXNE4X5',
-  secretAccessKey: '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',
-  region: 'us-east-1'
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.REGION,
 });
 
+// TODO - when something fails in function we need to delete whatever created earlier, like "certificates,things"
 const iot = new AWS.Iot();
-async function createThingAndAttachCerts (event) {
+const createThingAndAttachCerts = async (event) => {
+ var result={};
   try {
     // 1. Create new certificate
     const certRes = await new Promise((resolve, reject) => {
       const params = {
-        setAsActive: true
+        setAsActive: true,
       };
       iot.createKeysAndCertificate(params, function (err, data) {
         if (err) reject(err);
         else resolve(data);
       });
     });
-
-    console.log('certRes >> ', certRes);
-
+    result.certRes=certRes;
     // 2. Attaching policy to certificate
-
     const policyToCertRes = await new Promise((resolve, reject) => {
       iot.attachPolicy(
         {
           policyName: 'iotpolicy',
-          target: certRes.certificateArn
+          target: certRes.certificateArn,
         },
         (err, data) => {
           if (err) reject(err);
@@ -39,10 +35,8 @@ async function createThingAndAttachCerts (event) {
         }
       );
     });
-    console.log('policyToCertRes >> ', policyToCertRes);
-
+    result.policyToCertRes=policyToCertRes;
     // 3. Create Thing
-
     const thingRes = await new Promise((resolve, reject) => {
       iot.createThing(
         { thingName: event.iotDevice },
@@ -53,51 +47,26 @@ async function createThingAndAttachCerts (event) {
         }
       );
     });
-
-    console.log('thingRes >> ', thingRes);
-
+    result.thingRes=thingRes;
     // 4. Attach certificate to thing
-
     const attachedCertThingRes = await new Promise((resolve, reject) => {
       const params = {
         principal: certRes.certificateArn,
-        thingName: thingRes.thingName
+        thingName: thingRes.thingName,
       };
       iot.attachThingPrincipal(params, function (err, attachCertRes) {
         if (err) reject(err);
         else resolve(attachCertRes);
       });
     });
-
-    console.log('attachedCertThingRes >> ', attachedCertThingRes);
-
-    // return certRes;
-    return {
-      headers: {
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-        'Access-Control-Allow-Methods': '*'
-      },
-      statusCode: 200,
-      body: JSON.stringify(certRes)
-    };
+    result.attachedCertThingRes=attachedCertThingRes;
+    return result;
   } catch (e) {
     console.log(e);
-    return {
-      headers: {
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-        'Access-Control-Allow-Methods': '*'
-      },
-      statusCode: 200,
-      body: { message: e.message }
-    };
-  }
+    return result
 }
-
-// // this is to test locally
-// createThingAndAttachCerts({ iotDevice: '11:11:11:11:11:11' });
-
-export { createThingAndAttachCerts };
+}
+// let data =  createThingAndAttachCerts({ iotDevice: '11:11:11:11:11:11' })
+// console.log(data.then(result => {
+//     console.log(result)
+// }))
